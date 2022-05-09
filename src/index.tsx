bddbd6e12d247a06d9f6ebb3ad00dd373933e9f8
MaxAlter <alter0075926@gmail.com>
2022-05-09 13:12:08 +0300

    ADD | fix worker error

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
 export { ConnectButton } from "./walletHooks/component/ButtonConnect/connectButton";
 export { setStyles } from "./walletHooks/utils";
 export { useSignMessage } from "./walletHooks/component/hooks/useSignMessage";
-// export { useConnectors } from "./walletHooks/component/hooks/useConnectors";
