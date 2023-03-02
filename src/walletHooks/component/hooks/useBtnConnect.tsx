a6aec67a66bcc9b1363a0a67605ef1a70ce32c1d
MaxAlter <53145244+MaxAlter@users.noreply.github.com>
2023-03-02 15:56:32 +0200

    EDIT |  localStorage removeItem provider

--- src/walletHooks/component/hooks/useBtnConnect.tsx
+++ src/walletHooks/component/hooks/useBtnConnect.tsx
@@ -1,50 +1,51 @@
-import { useEffect, useState } from "react";
-import { useWeb3React } from "@web3-react/core";
-import { useConnectors } from "./useConnectors";
-import Web3 from "web3";
+import { useEffect, useState } from "react"
+import { useWeb3React } from "@web3-react/core"
+import { useConnectors } from "./useConnectors"
+import Web3 from "web3"
 
 export const useBtnConnect = () => {
-  const { library, account, activate, deactivate, active } = useWeb3React();
-  const { connectors } = useConnectors();
+  const { library, account, activate, deactivate, active } = useWeb3React()
+  const { connectors } = useConnectors()
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [balance, setBalance] = useState("0.00");
+  const [isOpen, setIsOpen] = useState(false)
+  const [balance, setBalance] = useState("0.00")
 
-  let web3 = new Web3(library?.provider);
+  let web3 = new Web3(library?.provider)
 
   const getInfo = async () => {
-    const balance = await web3.eth.getBalance(account + "");
+    const balance = await web3.eth.getBalance(account + "")
     if (balance) {
-      setBalance(balance);
+      setBalance(balance)
     }
-  };
+  }
 
   const openModal = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   const refreshState = () => {
-    window.localStorage.setItem("provider", "");
-  };
+    window.localStorage.setItem("provider", "")
+  }
 
   const disconnect = () => {
-    refreshState();
-    deactivate();
+    refreshState()
+    deactivate()
     // localStorage.clear();
-    window.localStorage.clear();
-  };
+    // window.localStorage.clear();
+    window.localStorage.removeItem("provider")
+  }
 
   useEffect(() => {
-    const provider = window.localStorage.getItem("provider");
+    const provider = window.localStorage.getItem("provider")
 
     if (provider) {
-      activate(connectors[provider]);
+      activate(connectors[provider])
     }
 
     if (account) {
-      getInfo();
+      getInfo()
     }
-  }, [account, active, activate]);
+  }, [account, active, activate])
 
   return {
     active,
@@ -53,5 +54,5 @@ export const useBtnConnect = () => {
     account,
     disconnect,
     isOpen,
-  };
-};
+  }
+}
